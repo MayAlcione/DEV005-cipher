958c786995c228cdf0af99e75ad3ec9197be5f5e
MayAlcione <myalcione@gmail.com>
2023-02-17 11:51:13 -0300

    Cambios en los codigos de indexjs

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
-};
+
+export function cifrar (texto, desplazamiento){
+  if (!texto)
+  return"";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+  let temp = (desplazamiento % 26 + 26) % 26;
+  if (temp == 0)
+    temp = 1;
+
+  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+temp)%26]);
+}
+
+ export function descifrar(texto, desplazamiento){
+  if (!texto)
+  return"";
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+  desplazamiento = (desplazamiento % 26 - 26) % 26;
+
+  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26]);
+}
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,62 +1,27 @@
+import {cifrar, descifrar} from './cipher.js';
+
 window.addEventListener("load", inicio, true);
 
 function inicio(){
     document.getElementById("mensaje").addEventListener("keyup", function(){
         this.value = this.value.toUpperCase();
     }, true);
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    
     document.getElementById("cifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensaje").value;
+        let texto = document.getElementById("mensaje").value.toUpperCase();
         let desplazamiento = document.getElementById("desplazamiento").value;
         document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
     }, true);
 
+    // Codigo de Decifrar
     document.getElementById("mensajes").addEventListener("keyup", function(){
         this.value = this.value.toUpperCase();
     }, true);
 
     document.getElementById("descifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensajes").value;
+        let texto = document.getElementById("mensajes").value.toUpperCase();
         let desplazamiento = document.getElementById("desplazamiento2").value;
         document.getElementById("mensaje3").value = descifrar(texto, desplazamiento);
     }, true);
 }
-
-function cifrar(texto, desplazamiento){
-    if (!texto)
-    return"";
-    const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-
-    desplazamiento = (desplazamiento % 26 + 26) % 26;
-
-    return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+desplazamiento)%26]);
-    }
-
-    function descifrar(texto, desplazamiento){
-        if (!texto)
-        return"";
-        const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-    
-        desplazamiento = (desplazamiento % 26 - 26) % 26;
-    
-        return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26]);
-        }
-     
 
