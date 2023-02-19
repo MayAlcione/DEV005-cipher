51af6017e13edfb5d307797948f2211c11c0f08c
MayAlcione <myalcione@gmail.com>
2023-02-19 18:35:26 -0300

    Imagen inserida

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,50 @@
+export function cifrar (mensaje, desplazamiento){
+  let mensajeCifrado = "";
 
-
-export function cifrar (texto, desplazamiento){
-  if (!texto)
-  return"";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-
-  let temp = (desplazamiento % 26 + 26) % 26;
-  if (temp == 0)
-    temp = 1;
-
-  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)+temp)%26]);
-}
-
- export function descifrar(texto, desplazamiento){
-  if (!texto)
-  return"";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
-
+  //convertir a entero desplazamiento
+  desplazamiento = parseInt(desplazamiento);
   
-  let temp = (desplazamiento % 26 - 26) % 26;
-  if (temp == 0)
-    temp = 1;
+  //como van correr las letras del mensaje
+  let aAscii;
+  for(let i = 0; i < mensaje.length; i++){
+    //sacar codigo Ascii  
+    aAscii = mensaje.charCodeAt(i);
+    let newAscii = ((aAscii - 65 + desplazamiento) % 26) + 65;
+    //para que Ascii solo llegue hasta 32, devolve lo mismo caracter
+    if (aAscii === 32,33,34,35,36,37,38,39,40,41,42,43,45,46,47) {newAscii = 32,33,34,35,36,37,38,39,40,41,42,43,45,46,47; }
 
-  desplazamiento = (desplazamiento % 26 - 26) % 26;
+    if (aAscii === 33) {newAscii = 33; }
+    
+    
+    let desdeAscii = String.fromCharCode(newAscii)
+    mensajeCifrado += desdeAscii
+  }
 
-  return texto.replace(/[A-Z]/ig, c=> letras[(letras.indexOf(c)-desplazamiento)%26]);
+  return mensajeCifrado
 }
 
 
+export function descifrar (mensaje, desplazamiento){
+  let mensajeDescifrado = "";
+
+  //convertir a entero desplazamiento
+  desplazamiento = parseInt(desplazamiento);
+  
+  //como van correr las letras del mensaje
+  let aAscii;
+  for(let i = 0; i < mensaje.length; i++){
+    //sacar codigo Ascii  
+    aAscii = mensaje.charCodeAt(i);
+    let newAscii = ((aAscii + 65 - desplazamiento) % 26) + 65;
+    //para que Ascii solo llegue hasta 32, que es un espacio para evitar otros caracteres
+    if (aAscii === 32) {newAscii = 32; }
+
+    if (aAscii === 33) {newAscii = 33; }
+    
+    
+    let desdeAscii = String.fromCharCode(newAscii)
+    mensajeDescifrado += desdeAscii
+  }
+
+  return mensajeDescifrado
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
-import {cifrar, descifrar} from './cipher.js';
-
+import {cifrar,descifrar} from './cipher.js';
+//para interacion del usuario con el navegador
 window.addEventListener("load", inicio, true);
 
+//funcionalidad de cambiar a mayusculas mientras digita
 function inicio(){
     document.getElementById("mensaje").addEventListener("keyup", function(){
         this.value = this.value.toUpperCase();
     }, true);
-    
+
+    //funcinalidad de boton cifrar
     document.getElementById("cifrar").addEventListener("click", function(){
-        let texto = document.getElementById("mensaje").value.toUpperCase();
+        //recibir mensaje
+        let texto = document.getElementById("mensaje").value;
+        //valor del desplazamiento
         let desplazamiento = document.getElementById("desplazamiento").value;
+        
         document.getElementById("mensaje2").value = cifrar(texto, desplazamiento);
     }, true);
+
 
     // codigo decifrar
     document.getElementById("mensajes").addEventListener("keyup", function(){
         this.value = this.value.toUpperCase();
     }, true);
 
-
     document.getElementById("descifrar").addEventListener("click", function(){
+
         let texto = document.getElementById("mensajes").value.toUpperCase();
+
         let desplazamiento = document.getElementById("desplazamiento2").value;
+
         document.getElementById("mensaje3").value = descifrar(texto, desplazamiento);
     }, true);
+    
 }
+
+
+inicio();
